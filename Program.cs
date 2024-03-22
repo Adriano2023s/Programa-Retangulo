983fc3ec7d47da16b3b333c8b4d5d58a3f2e05e1
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-03-22 15:16:10 -0300

    Programa sem classe

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CalculaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a altura do retângulo.");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a largura do retângulo");
+            double largura = double.Parse(Console.ReadLine());
+
+            double area = CalculaArea(altura, largura);
+            double diagonal = DiagonalRetangulo(altura, largura);
+            double perimetro = CalculaPerimetro(altura, largura);
+
+            Console.WriteLine($" A área do retângulo é de: {area}");
+            Console.WriteLine($"O perímetro do retângulo é de: {perimetro}");
+        }
+        public static double CalculaArea( double altura, double largura )
+        {
+            return altura * largura;
+        }
+        public static double DiagonalRetangulo( double altura, double largura )
+        {
+            return Math.Sqrt(Math.Pow(altura, 2) + Math.Pow(largura, 2));
+        }
+        public static double CalculaPerimetro( double altura, double largura )
+        {
+            return Math.Sqrt(Math.Pow(altura, 2) + Math.Pow(-largura, 2));
+        }
+    }
+}
