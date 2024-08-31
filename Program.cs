39b8b07ec2107f9ae4d468e027965b3c96d40f65
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-08-31 14:57:25 -0300

    Criado o programa, e adicionado o arquivo ignor

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramaRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
